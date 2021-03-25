f778e4606b586c3d279f44da998b6f5b83b4742f
Jordan Wilkerson <jordangwilkerson@gmail.com>
2021-03-25 11:49:13 -0600

    unique indexes

--- prompetition-code.server/server/services/TopicsService.js
+++ prompetition-code.server/server/services/TopicsService.js
@@ -7,12 +7,21 @@ class TopicsService {
     return topics
   }
 
+  async getTopicById(id) {
+    const topic = await dbContext.Topics.find({ _id: id })
+    return topic
+  }
+
   async createTopic(body) {
     return await dbContext.Topics.create(body)
   }
 
   async deleteTopic(id) {
     return await dbContext.Topics.findOneAndDelete({ _id: id })
+  }
+
+  async editTopic(id, userId, body) {
+    return await dbContext.Topics.findOneAndUpdate({ _id: id, creatorId: userId }, body, { new: true })
   }
 }
 export const topicsService = new TopicsService()
