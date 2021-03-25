eccc719af61e989535163800ce3502e900a58b9d
Ethan May <ethaniel.may@gmail.com>
2021-03-25 10:26:09 -0600

    More Images & Home Page Building

--- prompetition-code.client/src/pages/HomePage.vue
+++ prompetition-code.client/src/pages/HomePage.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="col-12">
-    <img src="https://raw.githubusercontent.com/JordanWilker/Prompetition/master/prompetition-code.client/src/assets/img/skybg.png" alt="Sky Background Logo">
+    <div class="row d-flex justify-content-center">
+      <img src="https://raw.githubusercontent.com/JordanWilker/Prompetition/master/prompetition-code.client/src/assets/img/skybg.PNG" alt="Sky Background Logo" class="img-fluid mt-3">
+    </div>
+    <div class="row d-flex justify-content-center mt-5">
+      <button class="btn btn-info px-5">
+        Daily Prompt
+      </button>
+      <button class="btn btn-dark">
+        <i class="fa fa-info-circle" aria-hidden="true"></i>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -11,12 +21,9 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home{
-  text-align: center;
-  user-select: none;
-  > img{
-    height: 200px;
-    width: 200px;
-  }
+img {
+  border-radius: 10px;
+  max-height: 207px;
+  filter: drop-shadow(2px 2px 2px rgba(0, 0, 0, 0.5));
 }
 </style>
