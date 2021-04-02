674d1ad9179d4904be22a4246a3dde2665326552
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-04-02 11:53:05 -0600

    Daily Challenge picks winners for past challenges

--- prompetition-code.server/server/services/TopicsService.js
+++ prompetition-code.server/server/services/TopicsService.js
@@ -1,4 +1,6 @@
 import { dbContext } from '../db/DbContext'
+import { logger } from '../utils/Logger.js'
+import { votesService } from './VotesService.js'
 // import { BadRequest } from '../utils/Errors'
 
 class TopicsService {
@@ -30,16 +32,38 @@ class TopicsService {
   }
 
   async getDailyChallenge() {
-    const date = new Date()
-    console.log(date)
+    const date = Date.parse(new Date().toDateString())
     // TODO: Only return the user's own response for the daily challenge
-    const res = await dbContext.Topics.findOne({ active: true, challengeStartDate: Date.parse(new Date().toDateString()) })
+    const oldTopic = await dbContext.Topics.find({ active: true, winnerResponseId: undefined })
+    for (let i = 0; i < oldTopic.length; i++) {
+      if (oldTopic[i].challengeStartDate !== date) {
+        const responses = await dbContext.Responses.find({ topicId: oldTopic[i].id })
+        // TODO: topic has no responses
+        let topScore = 0
+        let winners = []
+        for (let j = 0; j < responses.length; j++) {
+          const votes = await votesService.getVotesByResponseId(responses[j].id)
+          if (votes.length > topScore) {
+            topScore = votes.length
+            winners = [responses[j]._id]
+          } else if (votes.length === topScore) {
+            winners.push(responses[j]._id)
+          }
+        }
+        logger.log("Winners for yesterday's topic decided:\n", oldTopic[i].body)
+        logger.log('Winners:\n', winners)
+        oldTopic[i].winnerResponseId = winners
+        oldTopic[i].save()
+      }
+    }
+
+    const res = await dbContext.Topics.findOne({ active: true, challengeStartDate: date })
     if (res === null) {
       const topics = await dbContext.Topics.find({ active: false })
       const topicsLength = topics.length
       const topicPicker = Math.floor(Math.random() * topicsLength)
       topics[topicPicker].active = true
-      topics[topicPicker].challengeStartDate = Date.parse(new Date().toDateString())
+      topics[topicPicker].challengeStartDate = date
       topics[topicPicker].save()
       return topics[topicPicker]
     }
