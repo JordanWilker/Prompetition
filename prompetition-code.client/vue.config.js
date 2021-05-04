909d13fbbed66c7d2bf4cccf04b26f30dc658a29
Jordan Wilkerson <jordangwilkerson@gmail.com>
2021-05-04 11:50:49 -0600

    run build

--- prompetition-code.client/vue.config.js
+++ prompetition-code.client/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
   publicPath: '/',
-  outputDir: '../server/client',
+  outputDir: '../prompetition-code.server/client',
   productionSourceMap: false
 }
